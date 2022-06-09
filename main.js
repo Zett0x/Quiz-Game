809beb1298516b9bde0d3493e2059982673f07ef
franciscoiborra <franciscoiborra1997@gmail.com>
2022-06-09 17:55:37 +0200

    next question functionality

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const container = document.getElementById("answers");
 const answers = document.querySelectorAll(".answer");
 const title = document.getElementById("question");
 const scoreElement=document.getElementById("score");
+const pageElement=document.getElementById("page");
 console.log(scoreElement);
 
-
+let page=0;
 let score=0;
 const data = [
   {
+    usada:false,
     title: "¿Cuál de las siguientes opciones no es un editor de código?",
     repuestas: [
         { id: 1, res: "vim", correct: false },
@@ -16,13 +18,38 @@ const data = [
         { id: 4, res: "word", correct: true },
     ],
   },
+  {
+    usada:false,
+    title: "test 2",
+    repuestas: [
+        { id: 1, res: "a", correct: false },
+        { id: 2, res: "gasode", correct: false },
+        { id: 3, res: "eaggcs", correct: false },
+        { id: 4, res: "wgasg", correct: true },
+    ],
+  }
 ];
 
 console.log(data);
 //FUNCTIONS 
-const loadQuestion = () => {
-    title.textContent = data[0].title;
-    data[0].repuestas.forEach((q)=>{
+const clearAnswers=()=>{container.innerHTML=""};
+const nextQuestion=()=>{
+    if(page<data.length-1){
+        page++;
+    }
+    else page=0;
+    console.log(page);
+    clearAnswers();
+    loadQuestion(page);
+
+    
+
+}
+
+const loadQuestion = (page=0) => {
+    
+    title.textContent = data[page].title;
+    data[page].repuestas.forEach((q)=>{
         let answerDiv = document.createElement("div");
         answerDiv.textContent = q.res;
         answerDiv.classList.add("answer");
@@ -30,28 +57,43 @@ const loadQuestion = () => {
         container.appendChild(answerDiv);
     })
 };
+const loadPage=()=>{
+    pageElement.textContent=`Question ${page+1} of ${data.length}`
+}
+const updateScore=()=>{scoreElement.textContent=`Score: ${score}`;}
 
 const checkAnswer=(e)=>{
     if(e.target.id==="answers")
     return;
     let res=e.target.id;
-    if(data[0].repuestas[res-1].correct)
+    if(data[page].repuestas[res-1].correct /*&& !data[page].usada*/)
     {
         e.target.classList.add("correct");
-        scoreElement.textContent=`Score:${score}`;
         score++;
+        updateScore();
+
+        
 
     }
-    
     else
     e.target.classList.add("error");
+
+    //data[page].usada=true;
+
+    setTimeout(() => {
+        nextQuestion();
+    }, 800);
+    
     
 
 };
 
 
 //INIT
+loadPage();
+updateScore();
 loadQuestion();
+
 
 
 
